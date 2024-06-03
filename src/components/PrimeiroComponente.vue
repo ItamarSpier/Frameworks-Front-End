e635224df205e0b95e20a6771721f6ae67ca953b
ItamarSpier <itamarspier@gmail.com>
2024-06-03 19:59:48 -0400

    Add files via upload

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>SPIER INFORMATICA</h1>
+    <p>Meu nome é {{ nome }} e sou {{  profissao }} e {{ profissao2 }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'PrimeiroComponente',
+      data() {
+        return {
+            nome: "Itamar",
+            profissao: "TI",
+            profissao2: "programador"
+        }
+      }
+    }
+</script>
---
+++ src/components/Suporte.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+    <h3>Menu</h3>
+    <ul>
+      <li><a href="Link1" target="_blank" rel="noopener">Suporte</a></li>
+      <li><a href="Link2" target="_blank" rel="noopener">Sistema</a></li>
+    </ul>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
